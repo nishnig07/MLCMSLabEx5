06e0e67ef79f7706482074fbd411f46e16c91e4f
Aarav Malik <ge82jet@mytum.de>
2020-07-01 19:33:54 +0200

    Updates

--- src/Task_2_approximate_linear_vector_fields.py
+++ src/Task_2_approximate_linear_vector_fields.py
@@ -19,77 +19,106 @@ def approximate_linear_vector_fields(dataset_1_path, dataset_2_path):
     The approximation is calculated by:
     A^T= (X0^T.X0)^(−1).X0^T.V
 
-    # and the predicted values are:
-    # F_hat = X.A^T
-
     :param dataset_path: It is the path of the datasets needed.
     :return: none, plots the actual f values and approximated-f values on y-axis
              and x-values on x axis.
     """
 
+    """
+    Part1: Following code visualizes X0 and X1 and computes the the vector v as per equation
+    1.3 in the exercise sheet. We chose ∆t as .1
+    """
+
     names_x0 = ['x0_x', 'x0_y']
     data_x0 = pd.read_csv(dataset_1_path, sep=' ', names=names_x0)
     X0 = data_x0.to_numpy()
-    # plt.scatter(X0[:, 0], X0[:, 1], c='blue', alpha=.3)
+    plt.scatter(X0[:, 0], X0[:, 1], c='blue', alpha=.3)
 
     names_x1 = ['x1_x', 'x1_y']
     data_x1 = pd.read_csv(dataset_2_path, sep=' ', names=names_x1)
     X1 = data_x1.to_numpy()
-    # plt.scatter(X1[:, 0], X1[:, 1], c='red', alpha=.3)
+    plt.scatter(X1[:, 0], X1[:, 1], c='red', alpha=.3)
+    plt.show()
 
-    # for i in range(1000):
-    #     x, y = X0[i,0], X0[i,1]
-    #     u, v = X1[i, 0], X1[i, 1]
-    #     plt.quiver(x, y, u, v)
-    #     # plt.streamplot(x,y,u,v)
-    # plt.show()
+    V = (X1 - X0) / .1
 
-    V = (X1-X0)/.1
+    # x, y = np.meshgrid(np.linspace(-2, 2, 20), np.linspace(-2, 2, 20))
+    # u, v = np.zeros((20, 20)), np.zeros((20, 20))
+    # for i in range(0, 20):
+    #     for j in range(0, 20):
+    #         u[i, j] = V.T[0, i * 50]
+    #         v[i, j] = V.T[1, j * 50]
+    # plt.quiver(x, y, u, v)
+    # plt.streamplot(x, y, u, v)
+    plt.show()
+
+    """
+    Part2: Following code approximates and prints the A matrix (A_hat) using the following equation.
+    A^T= (X0^T.X0)^(−1).X0^T.V
+    We also compute X1 predictions using the approximated A_hat matrix 
+    and solve_ivp as differential equation solver to find X1 approximations at T=0.1
+    Using these values we calculateour Mean Squared Error (MSE)
+    """
+
     A_hat = (np.linalg.inv(X0.T @ X0) @ X0.T @ V).T
-    print(A_hat)
+    print("A_hat: " + str(A_hat))
 
-
-    # Ab = np.array([[-0.25, 0, 0],
-    #                [0.25, -0.2, 0],
-    #                [0, 0.2, -0.1]])
-
-    # time = np.array([0])
-    # A0 = np.array([10, 20, 30])
-
-    MA = []
+    X1_approx = []
     for i in range(X0.shape[0]):
-        sol = solve_ivp(fun=deriv, t_span=[0, 0.2], t_eval=[0.1], y0=X0[i,:] , args=(A_hat,))
-        MA.append(sol.y)
-    MA = np.array(MA)
-    MA = MA.reshape((1000,2))
+        sol = solve_ivp(fun=deriv, t_span=[0, 0.2], t_eval=[0.1], y0=X0[i, :], args=(A_hat,))
+        X1_approx.append(sol.y)
+    X1_approx = np.array(X1_approx)
+    X1_approx = X1_approx.reshape((1000, 2))
 
-    #MSE
-    MSE = np.square(X1-MA).mean()
+    # MSE
+    MSE = np.square(X1 - X1_approx).mean()
+    print("MSE: " + str(MSE))
 
-
-    # 3rd part
-    t_eval = [i/20 for i in range(100)]
-    sol = solve_ivp(fun=deriv, t_span=[0, 100], y0=[10,10], t_eval= t_eval, args=(A_hat,))
+    """
+    Part3: Following code deals with part 3 of of the task.
+    We compute first 100 points with with earlier chosen ∆t of 0.1
+    and plot them to show trajectory
+    We also compute points for first 100 seconds (Time step = 1)
+    and plot them to show trajectory
+    Then we plot the phase portrait of the trajectory.
+    """
+    t_eval = [i/10 for i in range(100)]  # For first 100 points (Evaluated at at .1,.2,.3,...)
+    sol = solve_ivp(fun=deriv, t_span=[0, 100], y0=[10, 10], t_eval=t_eval, args=(A_hat,))
     plt.scatter(sol.y[0, :], sol.y[1, :])
+    plt.xlabel("x_coordinate")
+    plt.ylabel("y_coordinate")
     plt.show()
 
     x, y = np.meshgrid(np.linspace(-10, 10, 20), np.linspace(-10, 10, 20))
     u, v = np.zeros((20, 20)), np.zeros((20, 20))
-    for i in range(0,20):
+    for i in range(0, 20):
         for j in range(0, 20):
-            u[i,j] = sol.y[0,i*5]
-            v[i,j] = sol.y[1,j*5]
-    plt.quiver(x,y,u,v)
-    plt.streamplot(x,y, u, v)
+            u[i, j] = sol.y[0, i * 5]
+            v[i, j] = sol.y[1, j * 5]
+    plt.quiver(x, y, u, v)
+    plt.streamplot(x, y, u, v)
     plt.show()
-    a = ''
 
+    t_eval = [i for i in range(100)] # For first 100 seconds (Evaluated at at 1,2,3,...)
+    sol = solve_ivp(fun=deriv, t_span=[0, 100], y0=[10, 10], t_eval=t_eval, args=(A_hat,))
+    plt.plot(sol.y[0, :], sol.y[1, :])
+    plt.xlabel("x_coordinate")
+    plt.ylabel("y_coordinate")
+    plt.show()
+
+    x, y = np.meshgrid(np.linspace(-10, 10, 20), np.linspace(-10, 10, 20))
+    u, v = np.zeros((20, 20)), np.zeros((20, 20))
+    for i in range(0, 20):
+        for j in range(0, 20):
+            u[i, j] = sol.y[0, i * 5]
+            v[i, j] = sol.y[1, j * 5]
+    plt.quiver(x, y, u, v)
+    plt.streamplot(x, y, u, v)
+    plt.show()
 
 
 if __name__ == '__main__':
     cwd = Path.cwd()
-    print(cwd)
     path = cwd
     approximate_linear_vector_fields(path / "datasets" / "linear_vectorfield_data_x0.txt",
                                      path / "datasets" / "linear_vectorfield_data_x1.txt")
-    # approximateData_LinearFunction(path / "linear_vectorfield_data_x1.txt")
